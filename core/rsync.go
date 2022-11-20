c27c366f9f6424bb77857e23e378bf0ab75dd750
Mirko Brombin <send@mirko.pm>
2022-11-20 23:29:34 +0100

    core: Support excluding paths with rsync

--- core/rsync.go
+++ core/rsync.go
@@ -21,14 +21,22 @@ func rsyncCmd(src, dst string, opts []string) error {
 	return cmd.Run()
 }
 
-func RsyncDryRun(src, dst string) error {
+func RsyncDryRun(src, dst string, excluded []string) error {
 	/*
 	 * rsyncDryRun executes the rsync command with the --dry-run option.
 	 */
+	opts := []string{"--dry-run"}
+
+	if len(excluded) > 0 {
+		for _, exclude := range excluded {
+			opts = append(opts, "--exclude="+exclude)
+		}
+	}
+
 	return rsyncCmd(src, dst, []string{"--dry-run"})
 }
 
-func AtomicRsync(src, dst string, keepUnwanted bool) error {
+func AtomicRsync(src, dst string, excluded []string, keepUnwanted bool) error {
 	/*
 	 * AtomicRsync executes the rsync command in an atomic-like manner.
 	 * It does so by dry-running the rsync, and if it succeeds, it runs
@@ -37,10 +45,18 @@ func AtomicRsync(src, dst string, keepUnwanted bool) error {
 	 * is set to true, it will omit the --delete option, so that the already
 	 * existing and unwanted files will not be deleted.
 	 */
-	if err := RsyncDryRun(src, dst); err != nil {
+	if err := RsyncDryRun(src, dst, excluded); err != nil {
 		return err
 	}
+
 	opts := []string{"--link-dest", dst + "/.link_dest"}
+
+	if len(excluded) > 0 {
+		for _, exclude := range excluded {
+			opts = append(opts, "--exclude", exclude)
+		}
+	}
+
 	if !keepUnwanted {
 		opts = append(opts, "--delete")
 	}
