8389e504ca6cbaa990eccae204a07c3504e8e687
Mirko Brombin <send@mirko.pm>
2022-11-20 23:31:01 +0100

    core: Add transaction methods and logs
    - NewTransaction
    - CancelTransaction
    - ApplyTransaction
    - TransactionalExec
    - NewTransactionalShell

--- core/transaction.go
+++ core/transaction.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -37,6 +38,7 @@ func UnlockTransaction() error {
 
 	err := os.Remove(lockPath)
 	if err != nil {
+		fmt.Printf("failed to remove lock file: %s", err)
 		return err
 	}
 
@@ -52,4 +54,101 @@ func AreTransactionsLocked() bool {
 	}
 
 	return true
+}
+
+func NewTransaction() error {
+	/*
+	 * NewTransaction starts a new transaction.
+	 * It does so by creating a new overlayfs with the current root as the
+	 * lower layer, and then locking the transactional shell.
+	 */
+	if AreTransactionsLocked() {
+		fmt.Println("Transactions are locked, another one is already running or a reboot is required.")
+		return nil
+	}
+
+	if err := NewOverlayFS([]string{"/"}); err != nil {
+		return err
+	}
+
+	if err := LockTransaction(); err != nil {
+		if err := CleanupOverlayPaths(); err != nil {
+			return err
+		}
+		return err
+	}
+
+	return nil
+}
+
+func CancelTransaction() error {
+	/*
+	 * CancelTransaction cancels the current transaction.
+	 * It does so by unlocking the transactional shell and unmounting the
+	 * overlayfs.
+	 */
+	if err := UnlockTransaction(); err != nil {
+		return err
+	}
+
+	if err := CleanupOverlayPaths(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ApplyTransaction() error {
+	/*
+	 * ApplyTransaction applies the current transaction.
+	 * It does so by merging the overlayfs into the future root, and then
+	 * updating the boot.
+	 */
+	if err := MountFutureRoot(); err != nil {
+		_ = CancelTransaction()
+		return err
+	}
+
+	if err := MergeOverlayFS("/partFuture"); err != nil {
+		_ = CancelTransaction()
+		return err
+	}
+
+	if err := UpdateRootBoot(true); err != nil {
+		_ = UnmountFutureRoot()
+		_ = CancelTransaction()
+		return err
+	}
+
+	if err := CleanupOverlayPaths(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TransactionalExec(command string) error {
+	/*
+	 * NewTransactionalShell starts a new transactional shell.
+	 * It does so by creating a new transaction, and then chrooting into the
+	 * overlayfs.
+	 */
+	if err := NewTransaction(); err != nil {
+		return err
+	}
+
+	if err := ChrootOverlayFS("", false, command); err != nil {
+		_ = CancelTransaction
+		return err
+	}
+
+	return nil
+}
+
+func NewTransactionalShell() error {
+	if err := TransactionalExec(""); err != nil {
+		return err
+	}
+
+	return nil
 }
