9cfe7401f884357c4d36d66b78eb14d1d8baebcc
Luca Di Maio <luca.dimaio1@gmail.com>
2022-11-21 23:05:19 +0100

    core: Add atomicSwap function, fix rsync command
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- core/rsync.go
+++ core/rsync.go
@@ -3,12 +3,12 @@ package core
 import (
 	"os"
 	"os/exec"
+
+	"golang.org/x/sys/unix"
 )
 
+// rsyncCmd executes the rsync command with the requested options.
 func rsyncCmd(src, dst string, opts []string) error {
-	/*
-	 * rsyncCmd executes the rsync command with the requested options.
-	 */
 	args := []string{"-a"}
 	args = append(args, opts...)
 	args = append(args, src)
@@ -21,10 +21,8 @@ func rsyncCmd(src, dst string, opts []string) error {
 	return cmd.Run()
 }
 
-func RsyncDryRun(src, dst string, excluded []string) error {
-	/*
-	 * rsyncDryRun executes the rsync command with the --dry-run option.
-	 */
+// rsyncDryRun executes the rsync command with the --dry-run option.
+func rsyncDryRun(src, dst string, excluded []string) error {
 	opts := []string{"--dry-run"}
 
 	if len(excluded) > 0 {
@@ -36,19 +34,41 @@ func RsyncDryRun(src, dst string, excluded []string) error {
 	return rsyncCmd(src, dst, []string{"--dry-run"})
 }
 
-func AtomicRsync(src, dst string, excluded []string, keepUnwanted bool) error {
-	/*
-	 * AtomicRsync executes the rsync command in an atomic-like manner.
-	 * It does so by dry-running the rsync, and if it succeeds, it runs
-	 * the rsync again performing changes. If the keepUnwanted option
-	 * is set to true, it will omit the --delete option, so that the already
-	 * existing and unwanted files will not be deleted.
-	 */
-	if err := RsyncDryRun(src, dst, excluded); err != nil {
+// atomicSwap allows swapping 2 files or directories in-place and atomically, using
+// the renameat2 syscall.
+func atomicSwap(src, dst string) error {
+	orig, err := os.Open(src)
+	if err != nil {
 		return err
 	}
 
-	opts := []string{}
+	new, err := os.Open(dst)
+	if err != nil {
+		return err
+	}
+
+	err = unix.Renameat2(int(orig.Fd()), src, int(new.Fd()), dst, unix.RENAME_EXCHANGE)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// AtomicRsync executes the rsync command in an atomic-like manner.
+// It does so by dry-running the rsync, and if it succeeds, it runs
+// the rsync again performing changes.
+// If the keepUnwanted option
+// is set to true, it will omit the --delete option, so that the already
+// existing and unwanted files will not be deleted.
+// To ensure the changes are applied atomically, we rsync on a _new directory first,
+// and use atomicSwap to replace the _new with the dst directory.
+func AtomicRsync(src, dst string, excluded []string, keepUnwanted bool) error {
+	if err := rsyncDryRun(src, dst+"_new", excluded); err != nil {
+		return err
+	}
+
+	opts := []string{"--link-dest", dst}
 
 	if len(excluded) > 0 {
 		for _, exclude := range excluded {
@@ -60,5 +80,15 @@ func AtomicRsync(src, dst string, excluded []string, keepUnwanted bool) error {
 		opts = append(opts, "--delete")
 	}
 
-	return rsyncCmd(src, dst, opts)
+	err := rsyncCmd(src, dst+"_new", opts)
+	if err != nil {
+		return err
+	}
+
+	err = atomicSwap(dst, dst+"_new")
+	if err != nil {
+		return err
+	}
+
+	return os.RemoveAll(dst + "_new")
 }
