8bb414a27256f6f91e9401417a3b9169c313a7b0
Mateus Melchiades <mateusbmelchiades@gmail.com>
2023-01-02 14:49:25 -0300

    Send kargs in update_boot cmd

--- cmd/update-boot.go
+++ cmd/update-boot.go
@@ -48,9 +48,13 @@ command should be used by advanced users for maintenance purposes.`) {
 		}
 	}
 
-	if err := core.UpdateRootBoot(false); err != nil {
-		return err
-	}
+    if kargs, err := core.GetKargs(); err != nil {
+        return err
+    } else {
+        if update_err := core.UpdateRootBoot(false, kargs); update_err != nil {
+            return update_err
+        }
+    }
 
 	return nil
 }
