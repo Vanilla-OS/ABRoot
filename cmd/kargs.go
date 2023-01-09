ec2d113da43b81ea396b8e5d51e165bc88c510fc
Mateus Melchiades <mateusbmelchiades@gmail.com>
2023-01-09 14:46:54 -0300

    Style: Reduce kargs command main func complexity

--- cmd/kargs.go
+++ cmd/kargs.go
@@ -74,50 +74,55 @@ func kargsCommand(cmd *cobra.Command, args []string) error {
 			fmt.Printf("Unknown state: %s\n", args[1])
 		}
 	case "edit":
-		editor := os.Getenv("EDITOR")
-		if editor == "" {
-			editor = "nano"
-		}
-
-		// Create custom kargs file if non-existent
-		if _, err := os.Stat(core.KargsPath); os.IsNotExist(err) {
-			cmd := exec.Command("cp", core.KargsDefaultPath, core.KargsPath)
-			if err := cmd.Run(); err != nil {
-				return err
-			}
-		}
-
-		// Open a temporary file, so editors installed via apx can also be used
-		cmd := exec.Command("cp", core.KargsPath, "/tmp/kargs-temp")
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-
-		// Call $EDITOR on temp file
-		cmd = exec.Command(editor, "/tmp/kargs-temp")
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-
-		// Copy temp file back to /etc
-		cmd = exec.Command("cp", "/tmp/kargs-temp", core.KargsPath)
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-
-		// Run something just to trigger a transaction
-		if _, err := core.TransactionalExec("echo"); err != nil {
-			fmt.Println("Failed to start transactional shell:", err)
-			os.Exit(1)
-		}
-
-		fmt.Println("Kernel parameters will be applied on next boot.")
+		kargs_edit()
 	default:
 		fmt.Printf("Unknown parameter: %s\n", args[0])
 	}
 
+	return nil
+}
+
+func kargs_edit() error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "nano"
+	}
+
+	// Create custom kargs file if non-existent
+	if _, err := os.Stat(core.KargsPath); os.IsNotExist(err) {
+		cmd := exec.Command("cp", core.KargsDefaultPath, core.KargsPath)
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	}
+
+	// Open a temporary file, so editors installed via apx can also be used
+	cmd := exec.Command("cp", core.KargsPath, "/tmp/kargs-temp")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	// Call $EDITOR on temp file
+	cmd = exec.Command(editor, "/tmp/kargs-temp")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	// Copy temp file back to /etc
+	cmd = exec.Command("cp", "/tmp/kargs-temp", core.KargsPath)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	// Run something just to trigger a transaction
+	if _, err := core.TransactionalExec("echo"); err != nil {
+		fmt.Println("Failed to start transactional shell:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Kernel parameters will be applied on next boot.")
 	return nil
 }
