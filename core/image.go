7f617abe3315864d1c168b7a44eb8eebfd5d476e
mirkobrombin <send@mirko.pm>
2023-04-16 11:15:49 +0200

    add ABImage struct

---
+++ core/image.go
@@ -0,0 +1,75 @@
+package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+// ABImage struct
+type ABImage struct {
+	Digest    string    `json:"digest"`
+	Timestamp time.Time `json:"timestamp"`
+	Image     string    `json:"image"`
+}
+
+// NewABImage returns a new ABImage struct
+func NewABImage(digest string, image string) *ABImage {
+	return &ABImage{
+		Digest:    digest,
+		Timestamp: time.Now(),
+		Image:     image,
+	}
+}
+
+// NewABImageFromRoot returns the current ABImage from /abimage.abr
+func NewABImageFromRoot() (*ABImage, error) {
+	PrintVerbose("NewABImageFromRoot: running...")
+
+	abimage, err := ioutil.ReadFile("/abimage.abr")
+	if err != nil {
+		PrintVerbose("NewABImageFromRoot:error: " + err.Error())
+		return nil, err
+	}
+
+	var a ABImage
+	err = json.Unmarshal(abimage, &a)
+	if err != nil {
+		PrintVerbose("NewABImageFromRoot:error(2): " + err.Error())
+		return nil, err
+	}
+
+	PrintVerbose("NewABImageFromRoot: found abimage.abr: " + a.Digest)
+	return &a, nil
+}
+
+// WriteTo writes the json to a dest path
+func (a *ABImage) WriteTo(dest string) error {
+	PrintVerbose("WriteTo: running...")
+
+	dir := filepath.Dir(dest)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		PrintVerbose("WriteTo:error: " + err.Error())
+		return err
+	}
+
+	imageName := fmt.Sprintf("abimage.abr")
+	imagePath := filepath.Join(dir, imageName)
+
+	abimage, err := json.Marshal(a)
+	if err != nil {
+		PrintVerbose("WriteTo:error(2): " + err.Error())
+		return err
+	}
+
+	err = ioutil.WriteFile(imagePath, abimage, 0644)
+	if err != nil {
+		PrintVerbose("WriteTo:error(3): " + err.Error())
+		return err
+	}
+
+	return nil
+}
