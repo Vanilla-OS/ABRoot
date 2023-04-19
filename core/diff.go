32617e02952ee086d4299ed7544d9d659b7c9f98
mirkobrombin <send@mirko.pm>
2023-04-19 14:46:01 +0200

    add methods to merge file diffs

---
+++ core/diff.go
@@ -0,0 +1,68 @@
+package core
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+)
+
+// MergeDiff merges the diff lines between source and dest
+func MergeDiff(sourceFile, destFile string) error {
+	PrintVerbose("MergeDiff: merging %s -> %s", sourceFile, destFile)
+
+	// get the diff lines
+	diffLines, err := DiffFiles(sourceFile, destFile)
+	if err != nil {
+		PrintVerbose("MergeDiff:error: %s", err)
+		return err
+	}
+
+	// write the diff to the destination
+	err = WriteDiff(destFile, diffLines)
+	if err != nil {
+		PrintVerbose("MergeDiff:error: %s", err)
+		return err
+	}
+
+	PrintVerbose("MergeDiff: merge completed")
+	return nil
+}
+
+// DiffFiles returns the diff lines between source and dest files.
+func DiffFiles(sourceFile, destFile string) ([]byte, error) {
+	PrintVerbose("DiffFiles: diffing %s -> %s", sourceFile, destFile)
+
+	cmd := exec.Command("diff", "-u", sourceFile, destFile)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		PrintVerbose("DiffFiles:error: %s", err)
+		return nil, err
+	}
+
+	PrintVerbose("DiffFiles: diff completed")
+	return out.Bytes(), nil
+}
+
+// WriteDiff applies the diff lines to the destination file.
+func WriteDiff(destFile string, diffLines []byte) error {
+	PrintVerbose("WriteDiff: applying diff to %s", destFile)
+	if len(diffLines) == 0 {
+		PrintVerbose("WriteDiff: no changes to apply")
+		return nil // no changes to apply
+	}
+
+	cmd := exec.Command("patch", destFile)
+	cmd.Stdin = bytes.NewReader(diffLines)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		PrintVerbose("WriteDiff:error: %s", err)
+		return err
+	}
+
+	PrintVerbose("WriteDiff: diff applied")
+	return nil
+}
