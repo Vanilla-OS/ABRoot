fca450f7750bbe141c337dfe68461e63b35f82fa
mirkobrombin <send@mirko.pm>
2023-04-19 14:45:09 +0200

    add methods to execute commands in a chroot

---
+++ core/chroot.go
@@ -0,0 +1,54 @@
+package core
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// ChrootExecute runs a command in a chroot environment
+func ChrootExecute(root string, cmd string, args []string) error {
+	PrintVerbose("ChrootExecute: running...")
+
+	// check if root exists
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		PrintVerbose("ChrootExecute:error: " + err.Error())
+		return err
+	}
+
+	// run command
+	cmd = filepath.Join(root, cmd)
+	cmd = strings.Join(append([]string{cmd}, args...), " ")
+	PrintVerbose("ChrootExecute: running command: " + cmd)
+	c := exec.Command("/bin/sh", "-c", cmd)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	c.Stdin = os.Stdin
+	err := c.Run()
+	if err != nil {
+		PrintVerbose("ChrootExecute:error: " + err.Error())
+		return err
+	}
+
+	PrintVerbose("ChrootExecute: successfully ran.")
+	return nil
+}
+
+// ChrootExecuteCmds runs a list of commands in a chroot environment,
+// stops at the first error
+func ChrootExecuteCmds(root string, cmds []string) error {
+	PrintVerbose("ChrootExecuteCmds: running...")
+
+	// run commands
+	for _, cmd := range cmds {
+		err := ChrootExecute(root, "/bin/sh", []string{"-c", cmd})
+		if err != nil {
+			PrintVerbose("ChrootExecuteCmds:error: " + err.Error())
+			return err
+		}
+	}
+
+	PrintVerbose("ChrootExecuteCmds: successfully ran.")
+	return nil
+}
