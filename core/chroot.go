cce937c1bfc57a946138a325382edd68568b989d
mirkobrombin <send@mirko.pm>
2023-04-22 17:58:19 +0200

    new Chroot struct
    this approach allow us bind-mounting essential paths. The Close() method ensure that all mount points are detached

--- core/chroot.go
+++ core/chroot.go
@@ -3,52 +3,92 @@ package core
 import (
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 )
 
-// ChrootExecute runs a command in a chroot environment
-func ChrootExecute(root string, cmd string, args []string) error {
-	PrintVerbose("ChrootExecute: running...")
-
-	// check if root exists
-	if _, err := os.Stat(root); os.IsNotExist(err) {
-		PrintVerbose("ChrootExecute:error: " + err.Error())
-		return err
-	}
-
-	// run command
-	cmd = filepath.Join(root, cmd)
-	cmd = strings.Join(append([]string{cmd}, args...), " ")
-	PrintVerbose("ChrootExecute: running command: " + cmd)
-	c := exec.Command("chroot", root, "/bin/sh", "-c", cmd)
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	c.Stdin = os.Stdin
-	err := c.Run()
-	if err != nil {
-		PrintVerbose("ChrootExecute:error: " + err.Error())
-		return err
-	}
-
-	PrintVerbose("ChrootExecute: successfully ran.")
-	return nil
+// Chroot is a struct which represents a chroot environment
+type Chroot struct {
+	root string
 }
 
-// ChrootExecuteCmds runs a list of commands in a chroot environment,
-// stops at the first error
-func ChrootExecuteCmds(root string, cmds []string) error {
-	PrintVerbose("ChrootExecuteCmds: running...")
+var mounts = []string{
+	"/dev",
+	"/dev/pts",
+	"/proc",
+	"/run",
+	"/sys",
+}
 
-	// run commands
-	for _, cmd := range cmds {
-		err := ChrootExecute(root, cmd, []string{})
+// NewChroot creates a new chroot environment
+func NewChroot(root string) (*Chroot, error) {
+	PrintVerbose("NewChroot: running...")
+
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		PrintVerbose("NewChroot:error: " + err.Error())
+		return nil, err
+	}
+
+	for _, mount := range mounts {
+		err := exec.Command("mount", "--bind", mount, root+mount).Run()
 		if err != nil {
-			PrintVerbose("ChrootExecuteCmds:error: " + err.Error())
+			PrintVerbose("NewChroot:error(2): " + err.Error())
+			return nil, err
+		}
+	}
+
+	return &Chroot{
+		root: root,
+	}, nil
+}
+
+// Close unmounts all the bind mounts
+func (c *Chroot) Close() error {
+	PrintVerbose("Close: running...")
+
+	for _, mount := range mounts {
+		err := exec.Command("umount", c.root+mount).Run()
+		if err != nil {
+			PrintVerbose("Close:error: " + err.Error())
 			return err
 		}
 	}
 
-	PrintVerbose("ChrootExecuteCmds: successfully ran.")
+	return nil
+}
+
+// Execute runs a command in the chroot environment
+func (c *Chroot) Execute(cmd string, args []string) error {
+	PrintVerbose("Execute: running...")
+
+	cmd = strings.Join(append([]string{cmd}, args...), " ")
+	PrintVerbose("Execute: running command: " + cmd)
+	e := exec.Command("chroot", c.root, "/bin/sh", "-c", cmd)
+	e.Stdout = os.Stdout
+	e.Stderr = os.Stderr
+	e.Stdin = os.Stdin
+	err := e.Run()
+	if err != nil {
+		PrintVerbose("Execute:error: " + err.Error())
+		return err
+	}
+
+	PrintVerbose("Execute: successfully ran.")
+	return nil
+}
+
+// ExecuteCmds runs a list of commands in the chroot environment,
+// stops at the first error
+func (c *Chroot) ExecuteCmds(cmds []string) error {
+	PrintVerbose("ExecuteCmds: running...")
+
+	for _, cmd := range cmds {
+		err := c.Execute(cmd, []string{})
+		if err != nil {
+			PrintVerbose("ExecuteCmds:error: " + err.Error())
+			return err
+		}
+	}
+
+	PrintVerbose("ExecuteCmds: successfully ran.")
 	return nil
 }
