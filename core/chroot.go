d0d8b2d1a7aa4e4f64b54d3a39d2610dc7c400f4
mirkobrombin <send@mirko.pm>
2023-04-23 11:18:39 +0200

    fix chroot not able to find the root device

--- core/chroot.go
+++ core/chroot.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,7 +13,7 @@ type Chroot struct {
 	rootUuid string
 }
 
-var mounts = []string{
+var ReservedMounts = []string{
 	"/dev",
 	"/dev/pts",
 	"/proc",
@@ -24,38 +25,51 @@ var mounts = []string{
 func NewChroot(root string, rootUuid string) (*Chroot, error) {
 	PrintVerbose("NewChroot: running...")
 
+	root = strings.ReplaceAll(root, "//", "/")
+
 	if _, err := os.Stat(root); os.IsNotExist(err) {
 		PrintVerbose("NewChroot:error: " + err.Error())
 		return nil, err
 	}
 
-	for _, mount := range mounts {
-		err := exec.Command("mount", "--bind", mount, root+mount).Run()
-		if err != nil {
-			PrintVerbose("NewChroot:error(2): " + err.Error())
-			return nil, err
-		}
+	chroot := &Chroot{
+		root:     root,
+		rootUuid: rootUuid,
 	}
 
-	// Workaround for a bug in os grub-probe, not detecting the
-	// root partition
-	err := exec.Command("mount", "-U", rootUuid, "/").Run()
+	// we need to mount /dev before the root so we can find the root device
+	err := exec.Command("mount", "--bind", "/dev", root+"/dev").Run()
+	if err != nil {
+		PrintVerbose("NewChroot:error(2): " + err.Error())
+		return nil, err
+	}
+
+	// workaround for a bug with grub-mkconfig not being able to find the
+	// root device
+	err = chroot.Execute("mount", []string{"-U", rootUuid, "/"})
 	if err != nil {
 		PrintVerbose("NewChroot:error(3): " + err.Error())
 		return nil, err
 	}
 
-	return &Chroot{
-		root:     root,
-		rootUuid: rootUuid,
-	}, nil
+	for _, mount := range ReservedMounts {
+		err := exec.Command("mount", "--bind", mount, root+mount).Run()
+		fmt.Println("mounting", mount, "to", root+mount)
+		if err != nil {
+			PrintVerbose("NewChroot:error(4): " + err.Error())
+			return nil, err
+		}
+	}
+
+	PrintVerbose("NewChroot: successfully created.")
+	return chroot, nil
 }
 
 // Close unmounts all the bind mounts
 func (c *Chroot) Close() error {
 	PrintVerbose("Close: running...")
 
-	for _, mount := range mounts {
+	for _, mount := range ReservedMounts {
 		err := exec.Command("umount", c.root+mount).Run()
 		if err != nil {
 			PrintVerbose("Close:error: " + err.Error())
