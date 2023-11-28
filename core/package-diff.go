9085f82ba8cebf2a8eb201b07b6129916a641f41
Mateus Melchiades <matbme@duck.com>
2023-11-28 00:08:10 +0000

    Fix BaseImagePackageDiff

--- core/package-diff.go
+++ core/package-diff.go
@@ -2,45 +2,61 @@ package core
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
-	"net/url"
+	"strings"
 
 	"github.com/vanilla-os/abroot/settings"
 )
 
 type PackageDiff struct {
-	Added, Removed struct {
-		Name, Version string
+	Added []struct {
+		Name       string
+		NewVersion string `json:"new_version"`
 	}
-	Updated, Downgraded struct {
-		Name, OldVersion, NewVersion string
+	Removed []struct {
+		Name       string
+		OldVersion string `json:"old_version"`
+	}
+	Upgraded, Downgraded []struct {
+		Name       string
+		OldVersion string `json:"new_version"`
+		NewVersion string `json:"old_version"`
 	}
 }
 
 func BaseImagePackageDiff(currentDigest, newDigest string) (PackageDiff, error) {
 	PrintVerbose("BaseImagePackageDiff: running...")
 
-	params := url.Values{}
-	params.Add("old_digest", currentDigest)
-	params.Add("new_digest", newDigest)
-	resp, err := http.Get(settings.Cnf.DifferURL + "?" + params.Encode())
+	imageComponents := strings.Split(settings.Cnf.Name, "/")
+	imageName := imageComponents[len(imageComponents)-1]
+	reqUrl := fmt.Sprintf("%s/images/%s/diff", settings.Cnf.DifferURL, imageName)
+	body := fmt.Sprintf("{\"old_digest\": \"%s\", \"new_digest\": \"%s\"}", currentDigest, newDigest)
+
+	request, err := http.NewRequest(http.MethodGet, reqUrl, strings.NewReader(body))
 	if err != nil {
 		PrintVerbose("PackageDiff.BaseImagePackageDiff:err: %s", err)
 		return PackageDiff{}, err
 	}
-	defer resp.Body.Close()
+	defer request.Body.Close()
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		PrintVerbose("PackageDiff.BaseImagePackageDiff(1):err: %s", err)
+		return PackageDiff{}, err
+	}
 
 	contents, err := io.ReadAll(resp.Body)
 	if err != nil {
-		PrintVerbose("PackageDiff.BaseImagePackageDiff(1):err: %s", err)
+		PrintVerbose("PackageDiff.BaseImagePackageDiff(2):err: %s", err)
 		return PackageDiff{}, err
 	}
 
 	diff := PackageDiff{}
 	err = json.Unmarshal(contents, &diff)
 	if err != nil {
-		PrintVerbose("PackageDiff.BaseImagePackageDiff(2):err: %s", err)
+		PrintVerbose("PackageDiff.BaseImagePackageDiff(3):err: %s", err)
 		return PackageDiff{}, err
 	}
 
