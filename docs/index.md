b28c7839f8c53d06dd5418b9fe46aed2acb4ba3b
mirkobrombin <send@mirko.pm>
2024-01-01 23:29:18 +0100

    feat: add godoc2md generated docs
    add index

---
+++ docs/index.md
@@ -0,0 +1,9 @@
+# ABRoot
+Development reference for ABRoot.
+
+## Table of Contents
+- [cmd](cmd.md)
+- [core](core.md)
+- [extras](extras.md)
+- [settings](settings.md)
+- [tests](tests.md)
--- docs/update.sh
+++ docs/update.sh
@@ -4,7 +4,6 @@ if [ $(basename $(pwd)) == "docs" ]; then
     cd ..
 fi
 
-godoc2md github.com/vanilla-os/abroot/core > docs/abroot.md
 godoc2md github.com/vanilla-os/abroot/core > docs/core.md
 godoc2md github.com/vanilla-os/abroot/core > docs/cmd.md
 godoc2md github.com/vanilla-os/abroot/core > docs/extras.md
