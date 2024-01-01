893e18375a0955f933c0100356aeb05405044483
mirkobrombin <send@mirko.pm>
2024-01-01 23:33:59 +0100

    feat: add godoc2md generated docs
    provide docs for all pkgs

--- docs/update.sh
+++ docs/update.sh
@@ -5,7 +5,7 @@ if [ $(basename $(pwd)) == "docs" ]; then
 fi
 
 godoc2md github.com/vanilla-os/abroot/core > docs/core.md
-godoc2md github.com/vanilla-os/abroot/core > docs/cmd.md
-godoc2md github.com/vanilla-os/abroot/core > docs/extras.md
-godoc2md github.com/vanilla-os/abroot/core > docs/settings.md
-godoc2md github.com/vanilla-os/abroot/core > docs/tests.md
+godoc2md github.com/vanilla-os/abroot/cmd > docs/cmd.md
+godoc2md github.com/vanilla-os/abroot/extras > docs/extras.md
+godoc2md github.com/vanilla-os/abroot/settings > docs/settings.md
+godoc2md github.com/vanilla-os/abroot/tests > docs/tests.md
